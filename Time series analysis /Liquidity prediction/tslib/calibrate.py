5bf6f0217f7b9b0dec75881aedc598a22d55144b
ProfessorBanner <121966431+ProfessorBanner@users.noreply.github.com>
2023-05-21 17:18:48 +0100

    Updated Time Series

---
+++ Time series analysis /Liquidity prediction/tslib/calibrate.py
@@ -0,0 +1,16 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import TimeSeriesSplit
+from tslib.scoring import get_score, get_score_estimator
+
+# Define a GridSearch based function to calibrate our model
+
+def calibrate_model(model, params, X, y):
+
+    model = GridSearchCV(model(),
+                         params,
+                         scoring=get_score_estimator,
+                         cv=TimeSeriesSplit(n_splits=5),
+                         n_jobs=-1)
+    model.fit(X, y)
+
+    return model
